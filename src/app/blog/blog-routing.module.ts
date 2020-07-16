597527eb424a4d99bb10b9b075f219aca762617b
Gary Barzola <gnbarzol@espol.edu.ec>
2020-07-15 20:57:03 -0500

    Add banner and mini-articles components for the blog

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BannerComponent } from './components/banner/banner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BannerComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
